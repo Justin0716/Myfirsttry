c59ab990559f9b96622759b3d0103f4785cdc2a1
劉晏齊 <justinliu0716@icloud.com>
2016-07-08 21:27:06 +0800

    Initial Commit

---
+++ firsttry/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  firsttry
+//
+//  Created by 劉晏齊 on 2016/7/8.
+//  Copyright © 2016年 Justin Liu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
